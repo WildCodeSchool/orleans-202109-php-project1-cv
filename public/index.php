60aa7d21c0fc04a717d8d2fe9364a565922466a2
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-10-04 23:10:30 +0200

    training init

--- public/index.php
+++ public/index.php
@@ -49,7 +49,7 @@ require '../src/skills.php';
             <p>Je maîtrise plein de choses ! Voici la liste de mes compétences principales.<br>
                 Mais pas de soucis, je sais aussi utiliser les photocopieurs et je fais un très bon café.</p>
 
-            <div class="skills_infos">
+            <div class="skills-cards">
                 <?php foreach ($skills as $skill) : ?>
                     <figure class="skill">
                         <img src="assets/images/skills/<?= $skill['image'] ?>" alt="">
@@ -60,14 +60,16 @@ require '../src/skills.php';
 
         </section>
 
-        <section id="trainings">
+        <section class="trainings" id="trainings">
             <h2>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16">
                     <path d="M8 .95 14.61 4h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.379l.5 2A.5.5 0 0 1 15.5 17H.5a.5.5 0 0 1-.485-.621l.5-2A.5.5 0 0 1 1 14V7H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 4h.89L8 .95zM3.776 4h8.447L8 2.05 3.776 4zM2 7v7h1V7H2zm2 0v7h2.5V7H4zm3.5 0v7h1V7h-1zm2 0v7H12V7H9.5zM13 7v7h1V7h-1zm2-1V5H1v1h14zm-.39 9H1.39l-.25 1h13.72l-.25-1z" />
                 </svg>
                 Formations
             </h2>
+            <div class="training-cards">
 
+            </div>
         </section>
     </main>
 
