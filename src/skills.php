3e75905e5e20f3f93d9ea6e75518609517aa42fa
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-10-04 16:47:34 +0200

    add skills dynamically in PHP

---
+++ src/skills.php
@@ -0,0 +1,9 @@
+<?php
+
+$skills = [
+    ['label' => 'Communication', 'image' => 'communication.png'],
+    ['label' => 'Project management', 'image' => 'project.png'],
+    ['label' => 'Analysis', 'image' => 'analysis.png'],
+    ['label' => 'Analysis', 'image' => 'analysis.png'],
+    ['label' => 'Planification', 'image' => 'planification.png'],
+];
