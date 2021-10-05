6cf05e2232047c63edb7de6839c24b861d0c77fc
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-10-05 11:03:35 +0200

    dojo

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 require '../src/skills.php';
+require '../src/trainings.php';
 ?>
 
 <!DOCTYPE html>
@@ -68,8 +69,30 @@ require '../src/skills.php';
                 Formations
             </h2>
             <div class="training-cards">
-
+                <?php foreach ($trainings as $training) : ?>
+                <div class="training-card">
+                    <img src="/assets/images/trainings/<?= $training['image'] ?>" />
+                    <div class="training-informations">
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar3" viewBox="0 0 16 16">
+                                <path d="M14 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" />
+                                <path d="M6.5 7a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-9 3a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm3 0a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
+                            </svg>
+                            <?= $training['date'] ?>
+                        </p>
+                        <h3><?= $training['title'] ?></h3>
+                        <p><?= $training['description'] ?></p>
+                        <p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bank" viewBox="0 0 16 16">
+                                <path d="M8 .95 14.61 4h.89a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5H15v7a.5.5 0 0 1 .485.379l.5 2A.5.5 0 0 1 15.5 17H.5a.5.5 0 0 1-.485-.621l.5-2A.5.5 0 0 1 1 14V7H.5a.5.5 0 0 1-.5-.5v-2A.5.5 0 0 1 .5 4h.89L8 .95zM3.776 4h8.447L8 2.05 3.776 4zM2 7v7h1V7H2zm2 0v7h2.5V7H4zm3.5 0v7h1V7h-1zm2 0v7H12V7H9.5zM13 7v7h1V7h-1zm2-1V5H1v1h14zm-.39 9H1.39l-.25 1h13.72l-.25-1z" />
+                            </svg>
+                            <?= $training['place'] ?>
+                        </p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
             </div>
+
         </section>
     </main>
 
---
+++ src/trainings.php
@@ -0,0 +1,26 @@
+<?php
+
+$trainings = [
+    [
+        'title' => 'Bachelor',
+        'date' => '2000',
+        'description' => 'lorem',
+        'image' => 'university1.webp',
+        'place' => 'MIT'
+    ],
+    [
+        'title' => 'Bachelor2',
+        'date' => '2001',
+        'description' => 'lorem2',
+        'image' => 'university2.webp',
+        'place' => 'MIT encore'
+    ],
+    [
+        'title' => 'Bachelor3',
+        'date' => '2000',
+        'description' => 'lorem',
+        'image' => 'university3.webp',
+        'place' => 'MIT'
+    ],
+
+];
